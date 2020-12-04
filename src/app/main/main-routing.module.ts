21e4d8a5417e2e88bd602a65fa8387ba2285117d
Yusuke Gunji <inochideutae@gmail.com>
2020-12-04 21:48:31 +0900

    init

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+    component: MainComponent,
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class MainRoutingModule {}
